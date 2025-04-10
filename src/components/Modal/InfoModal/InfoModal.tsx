ff9d746e5ee5b5b14d4731d600d80c1f61b69ebc
ehddk <bj9147@naver.com>
2025-04-10 21:51:52 +0900

    infoModal 생성

---
+++ src/components/Modal/InfoModal/InfoModal.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./InfoModal.module.scss";
+import cn from "classnames/bind";
+import Button from "@/components/NewButton/NewButton";
+import { IoMdClose } from "react-icons/io";
+
+const cx = cn.bind(styles);
+
+interface InfoModalProps {
+  onClose: () => void;
+  responsive?: boolean;
+}
+
+const InfoModal = (props: React.PropsWithChildren<InfoModalProps>) => {
+  const { children, onClose, responsive = false } = props;
+
+  return (
+    <div
+      className={cx("Wrapper", { responsive })}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <div className={cx("Header")}>
+        <Button.Root
+          className={cx("CloseBtn")}
+          variant="text"
+          colorType="inherit"
+          onClick={onClose}
+        >
+          <IoMdClose />
+        </Button.Root>
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default InfoModal;
