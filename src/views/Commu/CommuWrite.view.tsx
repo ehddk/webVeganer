7aa0bb385b037a828641722d6a8ae22ed7619d18
ehddk <bj9147@naver.com>
2025-05-01 22:02:52 +0900

    feat: 커뮤니티 페이지 수정중

--- src/views/Commu/CommuWrite.view.tsx
+++ src/views/Commu/CommuWrite.view.tsx
@@ -20,43 +20,49 @@ export default function CommuWriteView() {
   };
   return (
     <>
-      <div className={cx("Title")}>
-        <h3>글쓰기</h3>
-      </div>
-      <form action="/api/post/new" method="POST">
-        <div className={cx("Board")}>
-          <div className={cx("BoardTitle")}>
-            <input
-              name="title"
-              className={cx("BoardBody")}
-              placeholder="제목을 입력해주세요"
-            ></input>
-          </div>
-          <div className={cx("ListTop")}>
-            <ul style={{ listStyle: "none" }}>
-              <li>
-                <input type="file" accept="image/*" width={100} height={100} />
-                <button style={{ marginLeft: "30px" }}>장소</button>
-              </li>
-            </ul>
-          </div>
-          <div
-            style={{
-              margin: "0 0 0 80px",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <textarea name="content" className={cx("BoardContent")} />
-            <div className={cx("Con")}>
-              <button type="submit" className={cx("Btn")}>
-                등록하기
-              </button>
-              <button className={cx("Btn")}>취소</button>
+      <div className={cx("Wrapper")}>
+        <h2>글 작성</h2>
+
+        <form action="/api/post/new" method="POST">
+          <div className={cx("Board")}>
+            <div className={cx("BoardTitle")}>
+              <input
+                name="title"
+                className={cx("BoardBody")}
+                placeholder="제목을 입력해주세요"
+              ></input>
+            </div>
+            <div className={cx("ListTop")}>
+              <ul style={{ listStyle: "none" }}>
+                <li>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    width={100}
+                    height={100}
+                  />
+                  <button style={{ marginLeft: "30px" }}>장소</button>
+                </li>
+              </ul>
+            </div>
+            <div
+              style={{
+                //margin: "0 0 0 80px",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <textarea name="content" className={cx("BoardContent")} />
+              <div className={cx("Con")}>
+                <button type="submit" className={cx("Btn")}>
+                  등록하기
+                </button>
+                <button className={cx("Btn")}>취소</button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
