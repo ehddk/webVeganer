878686b30676d03d91b2ea46a3d14b284fc957a6
ehddk <bj9147@naver.com>
2025-06-06 12:40:14 +0900

    [김동아] modal 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,68 +6,33 @@ import styles from "./Modal.module.scss";
 import cn from "classnames/bind";
 const cx = cn.bind(styles);
 
-function Modal({ onCheck, onClose, onSelectedLocation }) {
+type ModalProps = {
+  onCheck?: boolean;
+  onClose?: () => void;
+  onSelectedLocation?: (location: string) => void;
+};
+function Modal(props: ModalProps) {
+  const { onSelectedLocation, onClose, onCheck } = props;
   //const [selectedLocation, setSelectedLocation] = useState(null);
   const [selectedSido, setSelectedSido] = useState(null);
   const [selectedSigugun, setSelectedSigugun] = useState(null);
   const [selectedDong, setSelectedDong] = useState(null);
   const [selectedCodeNm, setSelectedCodeNm] = useState(null);
 
-  const ModalContent = {
-    width: "700px",
-    height: "630px",
-    backgroundColor: "white",
-    borderRadius: "10px",
-    padding: "20px",
-    display: "flex",
-
-    // alignItems: "center",
-    position: "fixed",
-    top: "66px",
-    right: "446px",
-    // bottom: "190px",
-    // left: "380px"
-    // margin:"0 auto",
-  };
-  // const ModalButtons={
-  //     display:"flex",
-  //     justifyContent:"center",
-  //     marginTop:"500px"
-  // }
-  const CancelButton = {
-    width: "68px",
-    height: "29px",
-    borderRadius: "10px",
-    color: "black",
-    border: "none",
-    cursor: "pointer",
-    margin: "0 610px",
-  };
-
-  const title = {
-    width: "600px",
-    height: "40px",
-  };
-
-  const left = {
-    width: "620px",
-    height: "470px",
-  };
-
   const handleCity = () => {};
-  const save = {
-    width: "100px",
-    marginLeft: "270px",
-    backgroundColor: "navy",
-    color: "white",
-    border: "0",
-  };
+
   const handleSelectedLocation = (codeNm) => {
+    console.log("선택된 지역:", codeNm);
     setSelectedCodeNm(codeNm);
   };
   const handleSave = () => {
-    onSelectedLocation(selectedCodeNm);
-    onClose();
+    console.log("완료 버튼 클릭, 선택된 지역:", selectedCodeNm);
+    if (onSelectedLocation && selectedCodeNm) {
+      onSelectedLocation(selectedCodeNm);
+    }
+    if (onClose) {
+      onClose();
+    }
     // if (selectedSido && selectedSigugun  && selectedDong) {
     //     onSelectedLocation(selectedSido, selectedSigugun, selectedDong);
     //     onClose();
@@ -84,37 +49,30 @@ function Modal({ onCheck, onClose, onSelectedLocation }) {
   return (
     <div>
       <div className={cx("ModalContent")}>
-        <div style={{ margin: "0 auto" }}>
-          {/* <button style={CancelButton} onClick={onClose}>
-            X
-          </button> */}
+        <div>
           <div className={cx("Content")}>
-            <div style={title}>
+            <div>
               <p className={cx("Title")}>지역 선택</p>
             </div>
             <div>
-              <div style={left}>
-                <ul
-                  style={{
-                    listStyleType: "none",
-                    padding: "5px",
-                    marginTop: "3px",
-                    fontSize: "16px",
-                  }}
-                >
+              <div>
+                <ul className={cx("CityList")}>
                   <City
                     hangjungdong={hangjungdong}
                     onSelectLocation={handleSelectedLocation}
                     onSelectedDong={handleCitySelection}
-                    style={{ fontSize: "13px" }}
+                    // style={{ fontSize: "13px" }}
                     onClick={handleCity}
                   />
                 </ul>
               </div>
             </div>
-            <button style={save} onClick={handleSave}>
-              완료
-            </button>
+            <div className={cx("Button")}>
+              {" "}
+              <button className={cx("Save")} onClick={handleSave}>
+                완료
+              </button>
+            </div>
           </div>
         </div>
       </div>
