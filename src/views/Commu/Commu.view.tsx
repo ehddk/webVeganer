d887c2ed84dacef30d29cd9e389025d5e2b287bb
ehddk <bj9147@naver.com>
2025-07-02 11:55:57 +0900

    feat: 임시 데이터가 아닌 page단위에서 props로 내려준 data 사용

--- src/views/Commu/Commu.view.tsx
+++ src/views/Commu/Commu.view.tsx
@@ -1,49 +1,70 @@
 "use client";
 import styles from "./Commu.view.module.scss";
 import cn from "classnames/bind";
-import Table from "@/components/Table/Table";
-import Tables from "@/components/Tables/Table";
+
 import PostListTable from "./components/Table";
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { useRouter } from "next/navigation";
+import { articleTableColumns } from "./components/Table.columns";
+import useConditionalState from "@/hooks/useConditionalState";
 
 const cx = cn.bind(styles);
-export default function CommuView() {
-  const router = useRouter();
-  const table = useReactTable({
-    data: [
-      {
-        title: "제목",
-        content: "내용",
-        writer: "작성자",
-        createdAt: "2023-10-01",
-        view: 100,
-        category: "분류",
-      },
-    ],
 
-    columns: [
-      {
-        accessorKey: "category",
-        header: "분류",
-      },
-      {
-        accessorKey: "title",
-        header: "제목",
-      },
-      {
-        accessorKey: "writer",
-        header: "작성자",
-      },
-      {
-        accessorKey: "createdAt",
-        header: "작성일",
-      },
-      {
-        accessorKey: "view",
-        header: "조회수",
-      },
-    ],
+interface CommuViewProps {
+  data: Article.GetList.Response;
+  offset: number;
+  //sort:SortOreder;
+}
+
+export default function CommuView(props: CommuViewProps) {
+  const { data, offset } = props;
+  console.log("CommuView data", data);
+  const router = useRouter();
+  const [rowSelection, setRowSelection] = useConditionalState({}, {}, [
+    offset,
+    // sort,
+  ]);
+
+  const table = useReactTable({
+    data,
+    columns: articleTableColumns,
+    enableRowSelection: true,
+    state: {
+      rowSelection,
+    },
+    // data: [
+    //   {
+    //     title: "제목",
+    //     content: "내용",
+    //     writer: "작성자",
+    //     createdAt: "2023-10-01",
+    //     view: 100,
+    //     category: "분류",
+    //   },
+    // ],
+
+    // columns: [
+    //   {
+    //     accessorKey: "category",
+    //     header: "분류",
+    //   },
+    //   {
+    //     accessorKey: "title",
+    //     header: "제목",
+    //   },
+    //   {
+    //     accessorKey: "writer",
+    //     header: "작성자",
+    //   },
+    //   {
+    //     accessorKey: "createdAt",
+    //     header: "작성일",
+    //   },
+    //   {
+    //     accessorKey: "view",
+    //     header: "조회수",
+    //   },
+    // ],
     getCoreRowModel: getCoreRowModel(),
   });
   // const db = (await connectDB).db("vegan");
