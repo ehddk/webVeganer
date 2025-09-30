b14b354f0cedbad80d8fdf898a16caf3c28e4f0b
ehddk <bj9147@naver.com>
2025-09-30 23:02:39 +0900

    feat: 라우터 관련 constant 추가 및 view에 적용

---
+++ src/constants/link.constants.tsx
@@ -0,0 +1,27 @@
+const ROUTE_KEY = {
+  APP_DIR: "appDir",
+  URI: "uri",
+} as const;
+
+interface WithIdParams {
+  id?: string;
+}
+
+export const LINK_ROUTE = {
+  ARTICLE: {
+    DEFAULT: { [ROUTE_KEY.APP_DIR]: "/commu", [ROUTE_KEY.URI]: "/commu" },
+
+    DETAIL: {
+      [ROUTE_KEY.APP_DIR]: "/commu/detail/[id]",
+      [ROUTE_KEY.URI]: (params?: WithIdParams) => `/commu/${params?.id}`,
+    },
+    EDIT: {
+      [ROUTE_KEY.APP_DIR]: "/commu/Edit/[id]",
+      [ROUTE_KEY.URI]: "/commu/:id",
+    },
+    REGISTER: {
+      [ROUTE_KEY.APP_DIR]: "/commu/Register",
+      [ROUTE_KEY.URI]: "/commu/register",
+    },
+  },
+};
--- src/views/Commu/Commu.view.tsx
+++ src/views/Commu/Commu.view.tsx
@@ -32,39 +32,7 @@ export default function CommuView(props: CommuViewProps) {
     state: {
       rowSelection,
     },
-    // data: [
-    //   {
-    //     title: "제목",
-    //     content: "내용",
-    //     writer: "작성자",
-    //     createdAt: "2023-10-01",
-    //     view: 100,
-    //     category: "분류",
-    //   },
-    // ],
 
-    // columns: [
-    //   {
-    //     accessorKey: "category",
-    //     header: "분류",
-    //   },
-    //   {
-    //     accessorKey: "title",
-    //     header: "제목",
-    //   },
-    //   {
-    //     accessorKey: "writer",
-    //     header: "작성자",
-    //   },
-    //   {
-    //     accessorKey: "createdAt",
-    //     header: "작성일",
-    //   },
-    //   {
-    //     accessorKey: "view",
-    //     header: "조회수",
-    //   },
-    // ],
     getCoreRowModel: getCoreRowModel(),
   });
   // const db = (await connectDB).db("vegan");
