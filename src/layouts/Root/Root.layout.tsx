abe6bdf34fb1f4322368c13494facc39f0358d90
ehddk <bj9147@naver.com>
2025-11-01 22:24:48 +0900

    fix: 메인콘텐츠가 남은 영역을 차지하도록 css 수정

--- src/layouts/Root/Root.layout.tsx
+++ src/layouts/Root/Root.layout.tsx
@@ -1,24 +1,24 @@
-'use client";';
 import React from "react";
 import styles from "./Root.layout.module.scss";
 import cn from "classnames/bind";
 import Header from "@/components/Header/Header";
 import Footer from "@/app/Footer/Footer";
+import { cookies } from "next/headers";
 
 const cx = cn.bind(styles);
 
 type RootLayoutProps = React.PropsWithChildren<{}>;
 
-const RootLayout = (props: RootLayoutProps) => {
+const RootLayout = async (props: RootLayoutProps) => {
   const { children } = props;
+  const cookieStore = cookies();
+  const isAuthenticated = (await cookieStore).has("accessToken");
   return (
     <html lang="ko">
-      <body>
-        <div>
-          <Header session={undefined} />
-          <div className={cx("ContentContainer")}>{children}</div>
-          <Footer />
-        </div>
+      <body className={cx("Wrapper")}>
+        <Header session={{ isAuthenticated }} />
+        <div className={cx("ContentContainer")}>{children}</div>
+        <Footer />
       </body>
     </html>
   );
