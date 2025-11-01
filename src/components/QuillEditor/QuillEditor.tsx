a8460e2b15fac64490b96a8328a91e5142546cbc
ehddk <bj9147@naver.com>
2025-11-01 16:15:55 +0900

    feat: 에디터 컴포넌트로 분리

--- src/components/QuillEditor/QuillEditor.tsx
+++ src/components/QuillEditor/QuillEditor.tsx
@@ -35,3 +35,45 @@
 // });
 
 // export default QuillEditor;
+
+"use client";
+
+import React from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import cn from "classnames/bind";
+import styles from "./QuillEditor.module.scss"; // 경로는 실제 파일 위치에 맞게 수정
+
+const cx = cn.bind(styles);
+
+// Controller의 field와 기타 필요한 props를 받아오기 위한 interface
+interface QuillEditorProps {
+  value: any;
+  onChange: (content: any) => void;
+  className?: string;
+}
+
+export default function QuillEditor({
+  value,
+  onChange,
+  className,
+}: QuillEditorProps) {
+  // 실제 onChange 핸들러
+  const handleEditorChange = (
+    content: string,
+    delta: any,
+    source: string,
+    editor: any
+  ) => {
+    // Controller의 onChange에 Delta 객체를 전달
+    onChange(editor.getContents());
+  };
+
+  return (
+    <ReactQuill
+      value={value}
+      onChange={handleEditorChange}
+      className={cx("ContentEditor", className)}
+    />
+  );
+}
