270ad851ac9b4ec74666149661daef1d69158a07
ehddk <bj9147@naver.com>
2025-11-05 22:01:37 +0900

    feat: 서버 조회가 안될 경우에도 화면이 보이도록 error 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,16 @@ import HomeView from "@/views/Home/Home.view";
 
 export default async function HomePage() {
   const response = await RestaurantQuery.getList({});
-  if ("message" in response) throw new Error("조회 중 실패");
-  return <HomeView data={response} />;
+
+  let data: getRestaurantsResponse;
+
+  if ("message" in response) {
+    console.error("레스토랑 데이터 조회 중 서버 오류 발생:", response.message);
+    data = [];
+  } else {
+    // 정상적인 응답일 경우 데이터 사용
+    data = response;
+  }
+
+  return <HomeView data={data || []} />;
 }
