6079f63af54e7ea5fe9c8432cf1b8bf81a298973
ehddk <bj9147@naver.com>
2025-11-30 16:11:25 +0900

    [김동아] put 함수에 토큰 읽는 로직 추가

--- src/api/article/article.mutation.ts
+++ src/api/article/article.mutation.ts
@@ -7,11 +7,20 @@ import { ArticleService } from "./article.service";
 
 export const put = async (req: Article.Put.Request) => {
   try {
-    const data = await articleService.put(req);
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+
+    if (!accessToken) {
+      return { statusCode: 401, message: "로그인이 필요합니다" };
+    }
+
+    const serverAjax = createServerAjax(accessToken);
+    const serverArticleService = new ArticleService(serverAjax);
+
+    const data = await serverArticleService.put(req); // ★ 토큰 포함된 axios 사용
     return data;
   } catch (error) {
-    const data = await handleServerError(error);
-    return data;
+    return await handleServerError(error);
   }
 };
 export const post = async (req: Article.Post.Request) => {
