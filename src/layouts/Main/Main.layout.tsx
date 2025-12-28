2510eb5e61972bda98c642c2c4d9e4061b515d61
ehddk <bj9147@naver.com>
2025-12-28 16:06:50 +0900

    [김동아] 특정 path에 padding:0 적용 되도록 설정

--- src/layouts/Main/Main.layout.tsx
+++ src/layouts/Main/Main.layout.tsx
@@ -1,13 +1,28 @@
+"use client";
 import React from "react";
 import styles from "./Main.layout.module.scss";
 import cn from "classnames/bind";
+import { LINK_ROUTE } from "@/constants/link.constants";
+import { usePathname } from "next/navigation";
 const cx = cn.bind(styles);
 type MainLayoutProps = React.PropsWithChildren<{}>;
 
+const PATHS_WITHOUT_PADDING = [
+  LINK_ROUTE.RESTAURANT.DEFAULT.uri,
+  LINK_ROUTE.BRAND.uri,
+];
 const MainLayout = (props: MainLayoutProps) => {
   const { children } = props;
+  const pathname = usePathname() ?? "";
 
-  return <div className={cx("Wrapper")}>{children}</div>;
+  const noPadding = PATHS_WITHOUT_PADDING.some((path) =>
+    pathname.startsWith(path)
+  );
+  return (
+    <div className={cx("Wrapper", { "Wrapper-NoPadding": noPadding })}>
+      {children}
+    </div>
+  );
 };
 
 export default MainLayout;
