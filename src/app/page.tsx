f59e651d8b676176dfb627bcf7ca9a244ea16dde
ehddk <bj9147@naver.com>
2026-01-12 21:46:21 +0900

    feat: 메인페이지에 음식점 이미지 배열 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { RestaurantQuery } from "@/\bapi/query";
+import { fetchImagesForRestaurant } from "@/utils/imageCrawl";
 import HomeView from "@/views/Home/Home.view";
 
 export default async function HomePage() {
@@ -14,5 +15,21 @@ export default async function HomePage() {
     data = response;
   }
 
-  return <HomeView data={data || []} />;
+  const restaurants = response as Restaurant.GetList.Response;
+  if (restaurants.length === 0) {
+    return <p>등록된 식당이 없습니다.</p>;
+  }
+
+  const imagePromises = restaurants.map((restaurant) =>
+    fetchImagesForRestaurant(restaurant.upso_name)
+  );
+  const allImages = await Promise.all(imagePromises);
+
+  const dataWithImages = restaurants.map((restaurant, index) => ({
+    ...restaurant,
+    // 각 식당 객체에 해당 식당의 이미지 배열을 추가
+    initialBlogImages: allImages[index],
+  }));
+
+  return <HomeView data={dataWithImages || []} />;
 }
