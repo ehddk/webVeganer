ef3500c4286c59df78ae8b6bd3a8b228e735ad38
ehddk <bj9147@naver.com>
2026-01-14 21:20:09 +0900

    feat: 질문 constant 배열 생성

---
+++ src/constants/faq.constants.tsx
@@ -0,0 +1,27 @@
+export const FAQ_LIST = [
+  {
+    id: 1,
+    category: "basic",
+    question: "비건이란 무엇인가요?",
+  },
+  {
+    id: 2,
+    category: "menu",
+    question: "비건 초보자가 실패 안 하는 메뉴 추천해줘",
+  },
+  {
+    id: 3,
+    category: "basic",
+    question: "비건이랑 비건 옵션은 뭐가 다른가요?",
+  },
+  {
+    id: 4,
+    category: "menu",
+    question: "비건 음식점 가격대는 보통 어떤 편인가요?",
+  },
+  {
+    id: 5,
+    category: "concern",
+    question: "고기 먹는 사람도 같이 가도 괜찮은 곳이 있을까요?",
+  },
+];
