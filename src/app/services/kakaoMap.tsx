9a293ccae21e9ce0dadcfc94d2b3fe66ab1c8599
ehddk <bj9147@naver.com>
2026-02-10 20:50:52 +0900

    fix: build 에러 해결=> 구조분해할당부분 수정

--- src/app/services/kakaoMap.tsx
+++ src/app/services/kakaoMap.tsx
@@ -1,97 +1,3 @@
-// import React, { useEffect, useState, useRef } from "react";
-
-// declare global {
-//   interface Window {
-//     naver: any;
-//   }
-// }
-
-// const NAVER_CLIENT_ID = "jwjhld9nul";
-
-// function naverMap({ address }) {
-//   const [map, setMap] = useState(null);
-//   const mapRef = useRef(null);
-
-//   useEffect(() => {
-//     const initMap = () => {
-//       if (window.naver && mapRef.current) {
-//         const mapOptions = {
-//           center: new window.naver.maps.LatLng(33.450701, 126.570667),
-//           zoom: 14,
-//           // zoomControl: true,
-//           // zoomControlOptions: {
-//           //   position: window.naver.maps.Position.TOP_RIGHT,
-//           // },
-//         };
-
-//         const map = new window.naver.maps.Map(mapRef.current, mapOptions);
-//         setMap(map);
-
-//         //  주소가 있고 지오코딩 서비스가 로드되었다면 실행
-//         if (address && window.naver.maps.Service) {
-//           const geocoder = new window.naver.maps.Service.Geocoder();
-
-//           geocoder.geocode(
-//             {
-//               query: address,
-//             },
-//             function (status, response) {
-//               if (status === window.naver.maps.Service.Status.ERROR) {
-//                 return console.error("주소 변환 중 오류 발생");
-//               }
-
-//               if (response.v2.meta.totalCount > 0) {
-//                 const item = response.v2.addresses[0];
-//                 const point = new window.naver.maps.LatLng(item.y, item.x);
-
-//                 // 마커 생성
-//                 const marker = new window.naver.maps.Marker({
-//                   position: point,
-//                   map: map,
-//                 });
-
-//                 // 지도 중심 이동
-//                 map.setCenter(point);
-//               }
-//             }
-//           );
-//         }
-//       }
-//     };
-
-//     const loadNaverMapScript = () => {
-//       const isScriptExist = document.getElementById("naver-map-script");
-
-//       if (!isScriptExist) {
-//         const script = document.createElement("script");
-//         script.id = "naver-map-script"; // ⭐ id 부여
-//         script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=jwjhld9nul`;
-//         script.async = true;
-//         script.onload = initMap;
-//         document.head.appendChild(script);
-//       } else {
-//         // 이미 스크립트 있으면 바로 지도 초기화
-//         initMap();
-//       }
-//     };
-
-//     // 네이버 지도 API가 로드되었는지 확인
-//     if (window.naver && window.naver.maps) {
-//       initMap();
-//     } else {
-//       // 네이버 지도 API 로드
-//       loadNaverMapScript();
-//     }
-//     return () => {
-//       setMap(null);
-//     };
-//   }, [address]);
-
-//   return <div ref={mapRef} style={{ width: "100%", height: "500px" }} />;
-// }
-
-// export default naverMap;
-
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 
@@ -100,8 +6,11 @@ declare global {
     kakao: any;
   }
 }
+type KakaoMapProps = {
+  address: string;
+};
 
-function KakaoMap({ address }) {
+function KakaoMap({ address }: KakaoMapProps) {
   const mapRef = useRef(null);
   const [isMapLoaded, setIsMapLoaded] = useState(false);
 
@@ -141,29 +50,32 @@ function KakaoMap({ address }) {
       if (address) {
         const geocoder = new window.kakao.maps.services.Geocoder();
 
-        geocoder.addressSearch(address, function (result, status) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            const coords = new window.kakao.maps.LatLng(
-              result[0].y,
-              result[0].x
-            );
+        geocoder.addressSearch(
+          address,
+          function (result: { x: any; y: any }[], status: any) {
+            if (status === window.kakao.maps.services.Status.OK) {
+              const coords = new window.kakao.maps.LatLng(
+                result[0].y,
+                result[0].x
+              );
 
-            const marker = new window.kakao.maps.Marker({
-              map: map,
-              position: coords,
-            });
+              const marker = new window.kakao.maps.Marker({
+                map: map,
+                position: coords,
+              });
 
-            const infowindow = new window.kakao.maps.InfoWindow({
-              content:
-                '<div style="width:150px;text-align:center;padding:6px 0;">여기!</div>',
-            });
+              const infowindow = new window.kakao.maps.InfoWindow({
+                content:
+                  '<div style="width:150px;text-align:center;padding:6px 0;">여기!</div>',
+              });
 
-            infowindow.open(map, marker);
-            map.setCenter(coords);
-          } else {
-            console.error("주소를 찾을 수 없습니다:", address);
+              infowindow.open(map, marker);
+              map.setCenter(coords);
+            } else {
+              console.error("주소를 찾을 수 없습니다:", address);
+            }
           }
-        });
+        );
       }
     }
   }, [isMapLoaded, address]);
