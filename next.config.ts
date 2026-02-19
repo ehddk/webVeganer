5303e3cce43e4c0482cd608c81ea38976e007776
ehddk <bj9147@naver.com>
2026-02-19 22:38:38 +0900

    feat: supabase 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      // 배포 도메인도 추가 { hostname: "yourdomain.com" },
+    ],
+  },
 };
 
 export default nextConfig;
