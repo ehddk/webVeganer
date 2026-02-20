3a3b6d17dbea4a0b6c081d6171d8d264f7d746d9
ehddk <bj9147@naver.com>
2026-02-20 23:03:26 +0900

    feat: next/Image 컴포넌트 적용

--- src/components/RestaurantImage/RestaurantImage.tsx
+++ src/components/RestaurantImage/RestaurantImage.tsx
@@ -1,6 +1,6 @@
 import cn from "classnames/bind";
 import styles from "./RestaurantImage.view.module.scss";
-
+import Image from "next/image";
 const cx = cn.bind(styles);
 type RestaurantImageProps = {
   src?: string;
@@ -12,10 +12,14 @@ export const RestaurantImage = (props: RestaurantImageProps) => {
   const { src, alt, className } = props;
   const finalSrc = src || "/cafe.png";
   const isDefault = !src;
+
   return (
-    <img
+    <Image
       src={finalSrc}
       alt={alt}
+      width={400}
+      height={360}
+      sizes="(max-width: 600px) 100vw, 429px"
       className={cx("RestaurantImage", { isDefault: isDefault }, className)}
     />
   );
