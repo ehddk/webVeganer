668782b9775879bed51356eaa75645d6af59e573
ehddk <bj9147@naver.com>
2026-02-27 22:38:51 +0900

    콘솔 제거

--- src/app/commu/Edit/[id]/page.tsx
+++ src/app/commu/Edit/[id]/page.tsx
@@ -7,9 +7,9 @@ type CommuEditPageProps = PageProps<{
 }>;
 
 export default async function CommuEditPage(props: CommuEditPageProps) {
-  const { params, searchParams } = props;
+  const { params } = props;
   const { id } = await params;
-  console.log("idddd", id);
+
   const response = await ArticleQuery.getOne({ path: { id } });
   if ("message" in response) throw new Error("데이터 조회 중 실패");
   return <CommuEditView data={response} />;
