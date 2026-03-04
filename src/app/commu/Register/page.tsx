0382acf7c647b33e53e08ef734c88cf065c2b6ab
ehddk <bj9147@naver.com>
2026-03-04 22:21:15 +0900

    feat: 세션 정보 page.tsx에서 view로 전달

--- src/app/commu/Register/page.tsx
+++ src/app/commu/Register/page.tsx
@@ -1,7 +1,22 @@
 "use server";
 
+import { createSupabaseServerClient } from "@/lib/supabaseServer";
 import CommuRegisterView from "@/views/Commu/Register/CommuRegister.view";
 
 export default async function CommuRegisterPage() {
-  return <CommuRegisterView />;
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const session = {
+    user: user
+      ? {
+          id: user.id,
+          email: user.email,
+          name: user.user_metadata.name ?? "익명",
+        }
+      : null,
+  };
+
+  return <CommuRegisterView session={session} />;
 }
