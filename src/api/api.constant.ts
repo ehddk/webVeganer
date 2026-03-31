7e749b4b10453df23762b28aaeefb8930ba40a8a
ehddk <bj9147@naver.com>
2026-03-31 09:47:30 +0900

    fix: 환경변수로 적용

--- src/api/api.constant.ts
+++ src/api/api.constant.ts
@@ -1,12 +1,15 @@
 const isMock = false;
 const isDev =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "development" ? true : false;
-const isLocal = false;
+const isLocal = true;
 const localURL = "http://localhost:4000";
 
 export const API_MOCK_URL = "https://67a4467431d0d3a6b785c768.mockapi.io/";
 export const API_BASE_URL = (() => {
   if (isMock) return API_MOCK_URL;
   if (isLocal) return localURL;
-  return isDev ? "http://localhost:4000" : "https://veganerserver.onrender.com";
+  return (
+    process.env.NEXT_PUBLIC_API_URL || "https://veganerserver.onrender.com"
+  );
+  // ✅ 환경변수로 관리
 })();
