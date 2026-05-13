94d1638eeaab22a15b23e2cfa5ff1bf1c9afff6c
ehddk <bj9147@naver.com>
2026-05-13 20:10:05 +0900

    불필요한 주석 제거

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -11,53 +11,12 @@ export const baseAjax = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  // adapter: "fetch",
-  // fetchOptions: {
-  //   cache: "force-cache",
-  // },
+
   paramsSerializer(params) {
     return qs.stringify(params, { arrayFormat: "repeat" });
   },
 });
 
-//     try {
-//       let token;
-
-//       if (typeof window === "undefined") {
-//         // Server Environment
-//         try {
-//           const headers = await import("next/headers");
-//           const cookieStore = await headers.cookies();
-//           token = cookieStore?.get(STORAGE_KEY.ACCESS_TOKEN)?.value;
-//         } catch (e) {
-//           // headers API를 사용할 수 없는 경우 (generateStaticParams 등)
-//           return config;
-//         }
-//       } else {
-//         const Cookies = (await import("js-cookie")).default;
-
-//         // token =
-//         //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcwMzgxMjE0NiwiaWF0IjoxNzAxMjIwMTQ2LCJqdGkiOiI4YmNmNjNiNzYzOTk0ZmI2ODE3NDAwZDVjMzg4MzhkNCIsInVzZXJfaWQiOiJMa2JCUVY4RHd0ZiJ9.dDS9_aVvlVT6YB2tM65aVwSb4hYrryqynMe2xWlmpsI";
-//         token = Cookies.get(STORAGE_KEY.ACCESS_TOKEN);
-//       }
-
-//       if (token && !config.headers.Authorization) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//       }
-
-//       if (!token || config.headers["Auth"] === "false") {
-//         config.headers.Authorization = undefined;
-//         config.headers["Auth"] = undefined;
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//     return config;
-//   },
-//   async function (err) {
-//     return Promise.reject(err);
-//   }
-// );
 baseAjax.interceptors.request.use(
   async function (config) {
     // 브라우저 환경에서만 실행
